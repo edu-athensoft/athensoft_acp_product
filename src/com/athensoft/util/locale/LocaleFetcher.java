a26e90c044397eb7b2c156b643ad94eafc3c49d4
leon846666 <leon846666@gmail.com>
2018-05-12 23:07:17 -0400

    locale util

---
+++ src/com/athensoft/util/locale/LocaleFetcher.java
@@ -0,0 +1,35 @@
+package com.athensoft.util.locale;
+
+import java.util.Locale;
+
+import org.junit.experimental.theories.Theories;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import com.athensoft.ecomm.item.entity.ItemProductStatus;
+
+public class LocaleFetcher {
+	
+	  public static String getLocaleStr() {
+		  Locale locale = LocaleContextHolder.getLocale();  
+		  String localeStr=(locale.getLanguage()+"_"+locale.getCountry());
+		  localeStr =localToLangNo(localeStr);
+		  return localeStr;
+		  
+	}
+	  public static String localToLangNo(String localStr){
+			switch(localStr){
+			case "en_US": 
+				localStr="1033";
+				break;
+			case "fr_CA": 
+				localStr="3084";
+				break;
+			case "zh_CN": 
+				localStr="2052";
+				break;
+			default: 
+				break;
+	  }
+			  return localStr;
+	  }
+}
